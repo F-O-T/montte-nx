7b7f86e0b5d23e2546af049f6b93023d91e166e9
Manoel <manoel.carvalhoneto@tutamail.com>
2025-11-15 17:08:48 -0300

    feat: Remove english translations and i18next-http-backend

--- packages/localization/src/index.ts
+++ packages/localization/src/index.ts
@@ -1,7 +1,7 @@
 import i18n, { type TOptions } from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import ptBRResources from "./locales/pt-BR";
-
+import Backend from "i18next-http-backend";
 type RecursiveKeyOf<T> = T extends object
    ? {
         [K in keyof T]: K extends string
