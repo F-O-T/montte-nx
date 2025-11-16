6623c5fc40552909b86728632d3bdfbc943fc037
Manoel <manoel.carvalhoneto@tutamail.com>
2025-11-16 18:47:15 -0300

    refactor(database): Remove unused schema

--- packages/database/src/schema.ts
+++ packages/database/src/schema.ts
@@ -1,5 +1,4 @@
 export * from "./schemas/auth";
 export * from "./schemas/bank-accounts";
 export * from "./schemas/categories";
-export * from "./schemas/preferences";
 export * from "./schemas/transactions";
