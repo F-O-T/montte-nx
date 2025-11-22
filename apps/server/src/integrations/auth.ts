7fbfbf0e9c1e1be3d65f1f19117299ba05033c6e
Manoel <manoel.carvalhoneto@tutamail.com>
2025-11-22 16:27:48 -0300

    refactor: Remove unused payment package and client references

--- apps/server/src/integrations/auth.ts
+++ apps/server/src/integrations/auth.ts
@@ -1,16 +1,13 @@
 import { createAuth } from "@packages/authentication/server";
 import { serverEnv as env } from "@packages/environment/server";
-import { getPaymentClient } from "@packages/payment/client";
 import { getResendClient } from "@packages/transactional/client";
 import { openAPI } from "better-auth/plugins";
 import { db } from "./database";
 
 export const resendClient = getResendClient(env.RESEND_API_KEY);
-export const polarClient = getPaymentClient(env.POLAR_ACCESS_TOKEN);
 
 export const auth = createAuth({
    db,
-   polarClient,
    resendClient,
 });
 
