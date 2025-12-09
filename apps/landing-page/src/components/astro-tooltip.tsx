a6d72096dab0f9302598df8fc0f74a2c0292f0fc
Manoel <manoel.carvalhoneto@tutamail.com>
2025-12-09 06:42:20 -0300

    feat(landing-page): Improve logo cloud and navbar

---
+++ apps/landing-page/src/components/astro-tooltip.tsx
@@ -0,0 +1,21 @@
+import {
+   Tooltip,
+   TooltipContent,
+   TooltipTrigger,
+} from "@packages/ui/components/tooltip";
+
+interface AstroTooltipProps {
+   children: React.ReactNode;
+   content: React.ReactNode;
+}
+
+export function AstroTooltip({ children, content }: AstroTooltipProps) {
+   return (
+      <Tooltip>
+         <TooltipTrigger className="inline-block flex shrink-0 items-center justify-center">
+            {children}
+         </TooltipTrigger>
+         <TooltipContent>{content}</TooltipContent>
+      </Tooltip>
+   );
+}
